4322abc0151a5977325511a89a27e51ab57f9eb5
Balazs Kutil <balazs.kutil@gooddata.com>
2012-04-21 11:30:20 +0200

    Show warning if redis is down, refs #8

--- app/controllers/execution_controller.rb
+++ app/controllers/execution_controller.rb
@@ -1,7 +1,28 @@
 class ExecutionController < ApplicationController
   def index
-    @workers = Resque.workers
-    @schedule = Resque.schedule
-    @queues = Resque.queues
+    @workers = []
+    @schedule = []
+    @queues = []
+
+    @error = nil
+
+    begin
+      @workers = Resque.workers
+      @schedule = Resque.schedule
+      @queues = Resque.queues
+    rescue Errno::ECONNREFUSED => e
+      @error = e.message
+    end
+
+    respond_to do |format|
+      if @error
+        flash[:alert] = @error
+        format.html { render action: :index }
+        format.json { render json: @error, status: :unprocessable_entity }
+      else
+        format.html
+        format.json { render json: { workers: @workers, queues: @queues } }
+      end
+    end
   end
 end
